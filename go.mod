8ed4edaf280c7a1e9f0bcb57887c7b185456a398
Taner Özel <taner.ozel@primeapps.io>
2020-12-03 01:41:42 +0300

    database connection

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/codegangsta/negroni v1.0.0
 	github.com/coreos/go-oidc v2.2.1+incompatible
 	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.9.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
 	home v0.0.0
 	login v0.0.0
--- routes/user/user.go
+++ routes/user/user.go
@@ -1,19 +1,68 @@
 package user
 
 import (
-	"net/http"
-
 	"app"
+	"database/sql"
+	"fmt"
+	"net/http"
+	"os"
 	"templates"
+
+	_ "github.com/lib/pq"
 )
 
-func UserHandler(w http.ResponseWriter, r *http.Request) {
+type dataview struct{
+	Profile interface{} 
+	Names []string 
+}
+ 
 
+func UserHandler(w http.ResponseWriter, r *http.Request) {
+	
 	session, err := app.Store.Get(r, "auth-session")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	data:=&dataview{
+		Profile: session.Values["profile"],
+		Names :getUser(),
+	}
+	
+ 
+	templates.RenderTemplate(w, "user", data)
+}
 
-	templates.RenderTemplate(w, "user", session.Values["profile"])
+func getUser() []string{
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	username := os.Getenv("POSTGRES_USERNAME")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DATABASE_NAME")
+	connection_info := "host=" + host + " port=" + port + " user=" + username + " password=" + password + " dbname=" + dbname + " sslmode=disable"
+	 fmt.Print(connection_info)
+	db, err := sql.Open("postgres", connection_info)
+	if err != nil {
+		fmt.Println("Open Error")
+	}
+
+	rows, err := db.Query("SELECT * FROM Persons")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	var data= make([]string,0)
+
+	for rows.Next() {
+		var name string
+		_ = rows.Scan(&name)
+		data=append(data, name)
+		
+	}
+	if len(data) <1{
+		
+	if _, err := db.Exec("INSERT INTO Persons (name) VALUES ('FATIH SEVER'),('ÇAĞLAR SERT'),('BAHADIR CIVELEK'),('ATKAN GEMICI')"); err != nil {
+		fmt.Println(err.Error())
+	}
+	}
+	return data
 }
