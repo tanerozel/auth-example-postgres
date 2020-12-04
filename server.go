158691aa87686341b25269bd08c5613a23e07dc9
Taner Özel <taner.ozel@primeapps.io>
2020-12-04 21:27:59 +0300

    added healtz service

--- server.go
+++ server.go
@@ -4,15 +4,15 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/codegangsta/negroni"
-	"github.com/gorilla/mux"
-
 	"callback"
 	"home"
 	"login"
 	"logout"
 	"middlewares"
 	"user"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
 )
 
 func StartServer() {
@@ -26,6 +26,11 @@ func StartServer() {
 		negroni.HandlerFunc(middlewares.IsAuthenticated),
 		negroni.Wrap(http.HandlerFunc(user.UserHandler)),
 	))
+	r.HandleFunc("/healthz",func(response http.ResponseWriter, request *http.Request) {
+		message:="Service is run" 
+		response.Write([]byte(message))
+		
+	})
 	r.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("public/"))))
 	http.Handle("/", r)
 	log.Print("Server listening on http://localhost:3000/")
